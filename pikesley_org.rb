e1621c6cd344c80ebbbd88f93d51226ca7878fcf
pikesley <github@orgraphone.org>
2013-12-10 11:04:03 +0000

    Analytics tag

--- pikesley_org.rb
+++ pikesley_org.rb
@@ -3,20 +3,12 @@ require 'haml'
 require 'kramdown'
 require 'rack-google-analytics'
 
-
 class PikesleyOrg < Sinatra::Base
-  use Rack::GoogleAnalytics, :tracker => 'UA-20895204-12'
-
-  @@locals = {
-      :bootstrap_theme => 'http://bootswatch.com/united/bootstrap.min.css'
-  }
+  use Rack::GoogleAnalytics, :tracker => 'UA-46327971-1'
+  @@locals = { :bootstrap_theme => 'http://bootswatch.com/united/bootstrap.min.css' }
 
   get '/' do
-    haml :readme, :locals => @@locals.merge(
-        {
-            :title => 'sam.pikesley.org'
-        }
-    )
+    haml :readme, :locals => @@locals.merge({ :title => 'sam.pikesley.org' })
   end
 
   run! if app_file == $0
