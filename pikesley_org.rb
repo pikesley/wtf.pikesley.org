bacd6750aea0a2c4b1a05952d25e9f8e3b6e5426
pikesley <github@orgraphone.org>
2014-01-31 23:08:33 +0000

    Theme

--- pikesley_org.rb
+++ pikesley_org.rb
@@ -5,7 +5,7 @@ require 'rack-google-analytics'
 
 class PikesleyOrg < Sinatra::Base
   use Rack::GoogleAnalytics, :tracker => 'UA-46327971-1'
-  @@locals = { :bootstrap_theme => 'http://bootswatch.com/united/bootstrap.min.css' }
+  @@locals = { :bootstrap_theme => 'http://bootswatch.com/simplex/bootstrap.min.css' }
 
   get '/' do
     haml :readme, :locals => @@locals.merge({ :title => 'sam.pikesley.org' })
